b653e18ae8774e348aa6ac084273b038a6c90405
Ciprian Sasu <>
2024-09-05 13:43:35 +0300

    Remove unnecessary code

--- resources/lib/SFtpClient.php
+++ resources/lib/SFtpClient.php
@@ -28,42 +28,7 @@ class SFtpClient
     {
         return $this->sftp->login( $this->username, $this->password );
     }
-
-    /**
-     * Get the file names from the specified path
-     *
-     * @param string $path
-     *
-     * @return array
-     */
-    public function getFileNames( string $path ) : array
-    {
-        return $this->sftp->nlist( $path );
-    }
-
-    /**
-     * Get the content of a file
-     *
-     * @param string $fileName
-     *
-     * @return string|array
-     */
-    public function downloadFile( string $fileName ) : string
-    {
-        return $this->sftp->get( $fileName );
-    }
-
-    /**
-     * @param string $fileName
-     * @param string $newFileName
-     *
-     * @return string
-     */
-    public function renameFile( string $fileName, string $newFileName ) : string
-    {
-        //return $this->sftp->rename( $fileName, $newFileName );
-    }
-
+    
     /**
      * @param string $fileName
      * @param string  $content
@@ -111,36 +76,6 @@ class SFtpClient
             'error' => 'true',
             'error_msg' => 'could not write to SFTP server'
         ];
-    }
-
-    /**
-     * @param string $fileName
-     * @param string $content
-     *
-     * @return bool
-     */
-    public function uploadRawFile( string $fileName, $content ) : bool
-    {
-        if( $fp = fopen( 'php://temp', 'w+' ) )
-        {
-            try
-            {
-                fwrite( $fp, $content );
-                rewind( $fp );
-
-                return $this->sftp->put( $fileName, $fp, SFTP::SOURCE_LOCAL_FILE );
-            }
-            finally
-            {
-                fclose( $fp );
-            }
-        }
-    }
-
-
-    public function delete( string $path )
-    {
-        return $this->sftp->delete( $path );
     }
 
 }
