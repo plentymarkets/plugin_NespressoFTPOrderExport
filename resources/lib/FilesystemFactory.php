63e3d0bcd29ce2a645e12a02f010e087ca0cc171
Ciprian Sasu <>
2024-09-09 15:28:13 +0300

    Change library version

--- resources/lib/FilesystemFactory.php
+++ resources/lib/FilesystemFactory.php
@@ -1,7 +1,10 @@
 <?php
 
 use League\Flysystem\Filesystem;
-use League\Flysystem\Sftp\SftpAdapter;
+//use League\Flysystem\Sftp\SftpAdapter;
+use League\Flysystem\PhpseclibV3\SftpConnectionProvider;
+use League\Flysystem\PhpseclibV3\SftpAdapter;
+use League\Flysystem\UnixVisibility\PortableVisibilityConverter;
 
 /**
  * Class SdkFilesystemFactory
@@ -13,6 +16,7 @@ class FilesystemFactory
      */
     public static function create()
     {
+        /*
         $adapter = new SftpAdapter([
             'host' => SdkRestApi::getParam('host'),
             'port' => 22,
@@ -20,6 +24,33 @@ class FilesystemFactory
             'password' => SdkRestApi::getParam('password'),
             'timeout' => 100
         ]);
+        */
+        $adapter = new Filesystem(new SftpAdapter(
+            new SftpConnectionProvider(
+                SdkRestApi::getParam('host'), // host (required)
+                SdkRestApi::getParam('username'), // username (required)
+                SdkRestApi::getParam('password'), // password (optional, default: null) set to null if privateKey is used
+                null, // private key (optional, default: null) can be used instead of password, set to null if password is set
+                null, // passphrase (optional, default: null), set to null if privateKey is not used or has no passphrase
+                SdkRestApi::getParam('port'), // port (optional, default: 22)
+                true, // use agent (optional, default: false)
+                30, // timeout (optional, default: 10)
+                10, // max tries (optional, default: 4)
+                null, // host fingerprint (optional, default: null),
+                null, // connectivity checker (must be an implementation of 'League\Flysystem\PhpseclibV2\ConnectivityChecker' to check if a connection can be established (optional, omit if you don't need some special handling for setting reliable connections)
+            ),
+            '', // root path (required)
+            PortableVisibilityConverter::fromArray([
+                'file' => [
+                    'public' => 0640,
+                    'private' => 0604,
+                ],
+                'dir' => [
+                    'public' => 0740,
+                    'private' => 7604,
+                ],
+            ])
+        ));
 
         return new Filesystem($adapter);
     }
